fad2665b7ca60c6b4f6fe4c9d0d696d8862f82bd
subZero <marvinfordan08@gmail.com>
2026-01-11 18:45:10 +0800

    Add project files.

---
+++ TodoApp.Api/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; }
+}
---
+++ TodoApp.Api/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Api.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [MaxLength(100)]
+    public string Title { get; set; } = default!;
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
